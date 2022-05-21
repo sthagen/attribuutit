bd53067f317d77aa31d5a85b8a2705be5ababc36
Stefan Hagen <stefan@hagen.link>
2022-05-21 14:13:45 +0200

    Skeletal motion

---
+++ scan.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+"""Scan a tree of files and extract attributes of the underlying vector or raster data."""
+import sys
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def eject(name: str) -> int:
+    typer.echo(f"This will eject an example configuration as {name}.json")
+    return 0
+
+
+@app.command()
+def scan(name: str, persist: bool = True) -> int:
+    if persist:
+        typer.echo(f"Real run changing the world following the configuration {name}.json - later ...")
+        return 2	
+    else:
+        typer.echo(f"Dry run exposing the plan of execution when following the configuration {name}.json - later ...")
+        return 0
+
+    return 1
+
+
+if __name__ == "__main__":
+    sys.exit(app())
