71c54a9895166107ff11d205be5cd35e81c1a994
Stefan Hagen <stefan@hagen.link>
2022-05-22 12:13:15 +0200

    Boiler plate

---
+++ attribuutit/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
+import sys
+
+from attribuutit.cli import app
+
+if __name__ == '__main__':
+    sys.exit(app(prog_name='attribuutit'))  # pragma: no cover
