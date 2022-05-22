58de79694236e3a5b4800047e21bc3966e715d1e
Stefan Hagen <stefan@hagen.link>
2022-05-22 14:14:04 +0200

    Added inspect single file ad hoc command for prototyping

--- attribuutit/cli.py
+++ attribuutit/cli.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 """Scan a tree of files and extract attributes of the underlying vector or raster data."""
+import json
+import pathlib
 import sys
 
 import typer
+import attribuutit.shp as shp
+import attribuutit.tif as tif
+import attribuutit.vpf as vpf
 
 app = typer.Typer()
 
@@ -22,6 +27,27 @@ def scan(name: str, dry_run: bool = False) -> int:
         return 2
 
     typer.echo(f'Dry run exposing the plan of execution when following the configuration {name}.json - later ...')
+    return 0
+
+
+@app.command()
+def inspect(path_str: str, dry_run: bool = False) -> int:
+    """Inspect the file at path and persist the report eventually."""
+    if not dry_run:
+        typer.echo(f'Real run inspecting file at {path_str}')
+        path = pathlib.Path(path_str)
+        suffix = path.suffix
+        if suffix == '.shp':
+            shp.load(path)
+        elif suffix == '.tif':
+            tif.load(path)
+        else:
+            with open(path, 'rb') as source:
+                vpf_artifact = vpf.Table(str(path), (b for b in source.read()))
+            print(json.dumps(vpf_artifact.table, indent=2))
+        return 0
+
+    typer.echo(f'Dry run exposing the plan of execution when inspecting file at {path_str}')
     return 0
 
 
