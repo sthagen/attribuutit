2e2bed9ac069e7485789f6f85e659580737d5722
Stefan Hagen <stefan@hagen.link>
2022-05-22 12:31:23 +0200

    Added typiocraticized implementation - to reach a passing baseline

--- attribuutit/vpf.py
+++ attribuutit/vpf.py
@@ -1,6 +1,6 @@
 """Parsers for Vector Product Format (VPF) artifacts conforming to MIL-STD-2407."""
 from itertools import takewhile
-from typing import Dict, Tuple, Union, no_type_check
+from typing import Any, Dict, Generator, Tuple, Union, no_type_check
 
 EMPTY_FIELD_PLACEHOLDER = '-'
 VAR_LENGTH_INDICATOR = '*'
@@ -20,8 +20,8 @@ class ByteOrder:
     bo_indicator_index = 4
     head_off_expl = bo_indicator_index + 1  # Index of first header byte with byte order info present
 
-    @no_type_check
     @classmethod
+    @no_type_check
     def detect(cls, seq) -> Tuple[str, bool, int]:
         """Detect the endianness of the artifact and return byte order, declaration method, and next offset."""
         byte_order_explicit = True if chr(seq[cls.bo_indicator_index]) in cls.bo_keys else False
@@ -36,13 +36,14 @@ class HeaderLength:
 
     header_length_range = slice(0, 4)
 
-    @no_type_check
     @classmethod
+    @no_type_check
     def extract(cls, byte_order: str, seq) -> int:
         """Extract the length of the header for the artifact as encoded in the length field matching endianness."""
-        return int.from_bytes(seq[cls.header_length_range], byteorder=byte_order)
+        return int.from_bytes(seq[cls.header_length_range], byteorder=byte_order)  # noqa
 
 
+@no_type_check
 class Table:
     """Type to parse a VPF table like artifact - eg. a Database Header Table (dht)."""
 
@@ -55,14 +56,15 @@ class Table:
     table: Dict[str, Union[int, object, str]] = {}
     ERROR = -1
 
-    def __init__(self, label: str, byte_stream: bytearray) -> None:
+    @no_type_check
+    def __init__(self, label: str, byte_stream: Generator[bytes, Any, None]) -> None:
         """Parse the table data from the stream of bytes."""
         self.label = label
         self._seq = list(byte_stream)  # Eager consumption of the stream (for now)
 
         self.bootstrap_table()
 
-        next_segment_start = self.parse_table_description(self.table['header_byte_offset'] + 1)  # type: ignore
+        next_segment_start = self.parse_table_description(self.table['header_byte_offset'] + 1)
         if next_segment_start == self.ERROR:
             return
 
@@ -74,6 +76,7 @@ class Table:
         _ = self.parse_columns(next_segment_start)  # hand over ... an start of next segment or self.ERROR
         return
 
+    @no_type_check
     def bootstrap_table(self) -> None:
         """Detect byte order (endianness) from and determine header length of artifact."""
         byte_order, byte_order_explicit, header_byte_offset = ByteOrder.detect(self._seq)
@@ -86,6 +89,7 @@ class Table:
             'header_byte_offset': header_byte_offset,
         }
 
+    @no_type_check
     def parse_table_description(self, next_segment_start: int) -> int:
         """Parse the name of the table description and return next segment start offset."""
         rem_seq = self._seq[next_segment_start:]
@@ -99,8 +103,9 @@ class Table:
             self.table['error_detail'] = f'failing to parse table description with {err}'
             return self.ERROR
 
-        return self.table['header_byte_offset'] + 1 + len(table_description) + 1  # type: ignore
+        return self.table['header_byte_offset'] + 1 + len(table_description) + 1
 
+    @no_type_check
     def parse_narrative_table_name(self, next_segment_start: int) -> int:
         """Parse the name of the narrative table and return next segment start offset."""
         rem_seq = self._seq[next_segment_start:]
@@ -114,6 +119,7 @@ class Table:
 
         return next_segment_start + len(narrative_table_name) + 1
 
+    @no_type_check
     def parse_columns(self, next_segment_start: int) -> int:
         """Parse all available column specs and return next segment start offset."""
         eq_chr = '='
@@ -121,7 +127,7 @@ class Table:
         colon_chr = ':'
         col_rank = 0
         rem_seq = self._seq[next_segment_start:]
-        while rem_seq and chr(rem_seq[0]) != self.semi_chr:
+        while rem_seq and chr(rem_seq[0]) != self.semi_chr:  # noqa
             col_rank += 1
             column_spec = ''.join(chr(c) for c in takewhile(lambda x: chr(x) != colon_chr, rem_seq))
             try:
@@ -141,7 +147,7 @@ class Table:
                     optional_thematic_index_name,
                     optional_column_narrative_table_name,
                 ) = spec.rstrip(comma_chr).split(comma_chr)
-                self.table['columns'][col_name] = {  # type: ignore
+                self.table['columns'][col_name] = {
                     'column_rank': col_rank,
                     'column_name': col_name,
                     'field_type': field_type,
@@ -156,7 +162,7 @@ class Table:
                 self.table['error'] = True
                 self.table['error_detail'] = f'failing to parse {col_name} column spec with {err}'
                 return self.ERROR
-            that_key_type = self.table['columns'][col_name]['key_type']  # type: ignore
+            that_key_type = self.table['columns'][col_name]['key_type']
             if that_key_type not in self.key_types:
                 self.table['error'] = True
                 self.table[
