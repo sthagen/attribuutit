5a4eb7a983e9577d2ebbb1ac4e8c8f659aae2740
Stefan Hagen <stefan@hagen.link>
2022-06-29 22:30:32 +0200

    Fixed and extended out-of-sync test

--- test/test_shp.py
+++ test/test_shp.py
@@ -7,7 +7,19 @@ SHP_ROOT = pathlib.Path('local', 'incoming')
 
 def test_shp_load(capsys):
     path = SHP_ROOT / 'ne_110m_admin_0_tiny_countries.shp'
-    result = shp.load(path)
-    assert result is None
+    error, result = shp.load(path)
+    assert not error
+    assert result['feature_count'] == 37
+    assert result['geo_json_type'] == 'FeatureCollection'
+    bbox = result['bounding_box']
+    assert len(bbox) == 4
+    # [-175.23533295466754, -54.274478863695265, 179.20397422623353, 71.02824880643254]
+    assert bbox[0] <= bbox[2]
+    assert bbox[1] <= bbox[3]
+    assert -180 <= bbox[0] <= 180
+    assert -180 <= bbox[2] <= 180
+    assert -90 <= bbox[1] <= 90
+    assert -90 <= bbox[3] <= 90
     out, err = capsys.readouterr()
-    assert 'bbox: ' in out
+    assert not out
+    assert not err
