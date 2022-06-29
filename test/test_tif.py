bb795c88c7d6dad4218327512bb670e6d64496d7
Stefan Hagen <stefan@hagen.link>
2022-06-29 22:50:28 +0200

    Fixed out of sync test

--- test/test_tif.py
+++ test/test_tif.py
@@ -7,7 +7,18 @@ TIF_ROOT = pathlib.Path('local', 'incoming')
 
 def test_tif_load(capsys):
     path = TIF_ROOT / 'GRAY_50M_SR_OB.tif'
-    result = tif.load(path)
-    assert result is None
+    error, result = tif.load(path)
+    assert not error
+    assert result['crs_name'] == 'WGS_84'
+    assert result['crs_code'] == 4326
+    bbox = result['bounding_box']
+    assert len(bbox) == 4
+    assert bbox[0] <= bbox[2]
+    assert bbox[1] >= bbox[3]
+    assert -180 <= bbox[0] <= 180
+    assert -180 <= bbox[2] <= 180
+    assert -90 <= bbox[1] <= 90
+    assert -90 <= bbox[3] <= 90
     out, err = capsys.readouterr()
-    assert 'crs_name: WGS_84 (code 4326)' in out
+    assert not out
+    assert not err
