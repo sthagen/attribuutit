fe330fa1d102370351cb3d3d1ad6d086ca9fe74d
Stefan Hagen <stefan@hagen.link>
2025-07-08 02:33:44 +0200

    Bumped impl version
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- attribuutit/__init__.py
+++ attribuutit/__init__.py
@@ -18,8 +18,8 @@ DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.1+parent.7635e35c'
-# [[[end]]] (checksum: 336a0105003ee28994a568480df2fbd9)
+__version__ = '2025.7.8+parent.39c6085c'
+# [[[end]]] (checksum: 19acc403bc04eec8c4e4f1adbd3c3415)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
